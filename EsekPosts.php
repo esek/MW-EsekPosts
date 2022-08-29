181db744167c50aa7d242b761aa16043cd822379
Axel Froborg <axel.froborg@gmail.com>
2022-08-29 20:04:47 +0200

    commit

--- EsekPosts.php
+++ EsekPosts.php
@@ -2,17 +2,32 @@
 
 class EsekPosts
 {
-	// Register any render callbacks with the parser
+	const TAG_NAME = 'Posts';
+
+	// gets run on the `ParserFirstCallInit` hook
 	public static function onParserFirstCallInit(Parser $parser)
 	{
-		// When the parser sees the <sample> tag, it executes renderTagSample (see below)
-		$parser->setHook('Posts', [self::class, 'renderPosts']);
+		// When the parser sees the <Posts> tag, it executes renderPosts
+		$parser->setHook(self::TAG_NAME, [self::class, 'renderPosts']);
 	}
 
-	// Render <sample>
 	public static function renderPosts($input, array $args, Parser $parser, PPFrame $frame)
 	{
-		// Nothing exciting here, just escape the user-provided input and throw it back out again (as example)
-		return htmlspecialchars($input);
+		// disable the cache for this extension
+		$parser->disableCache();
+
+		// if no STIL-ID, don't return anything
+		if (empty($input)) {
+			return '';
+		}
+
+		$username = htmlspecialchars($input);
+
+		return "<p>Denna funktionalitet har flyttat <a href=\"https://esek.se/members/@${$username}\" target=\"_blank\">hit!</a></p>";
+	}
+
+	public static function fetchPosts()
+	{
+		// TODO: Fetch the actual posts
 	}
 }
